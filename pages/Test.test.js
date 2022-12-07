d908e5d3f323c86b3dbf8fc8d15cebe41716f4aa
RepolloEzzz <gabrielrepolloez@gmail.com>
2022-12-07 11:29:13 -0400

    meta y lang en general

--- pages/Test.test.js
+++ pages/Test.test.js
@@ -11,16 +11,8 @@ export default function Test() {
     <>
     <Head>
         <title>TeCitoHot - Repositorio Puro</title>
-        <meta charset="UTF-8"/>
-        <meta name="author" content="@RepolloEzzz"/>
+        
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content='Respitorio de materias que se ven en la universidad o simplemente temas que son relevantes en algunos aspectos. Este sitio tiene contenido que en otras páginas no encontraras o en su defecto encontraras más de lo que
-        quieres😳'/>
-        <meta name="keywords" content="Temas de matemáticas, Temas de física, Temas de programación"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/title/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/title/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/titlle/favicon-16x16.png"/>
-        <link rel="manifest" href="/icons/title/site.webmanifest"></link>
       </Head>
       <section className="flex">
         <HeroNavV
